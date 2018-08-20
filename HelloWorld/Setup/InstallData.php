9de511829a3b680be5895d5aa7278b2b0b6173a1
XMars <mars@trueplus.vn>
2018-08-20 14:42:56 +0700

    [ADD] new attribute for category

---
+++ HelloWorld/Setup/InstallData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Magestore\HelloWorld\Setup;
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(
+        EavSetupFactory $eavSetupFactory
+    )
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(
+        ModuleDataSetupInterface $setup, ModuleContextInterface $context
+    )
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'mgs_hot_attribute',
+            [
+                'type'         => 'varchar',
+                'label'        => 'Hot hon hat Attribute',
+                'input'        => 'text',
+                'sort_order'   => 100,
+                'source'       => '',
+                'global'       => 1,
+                'visible'      => true,
+                'required'     => false,
+                'user_defined' => false,
+                'default'      => null,
+                'group'        => '',
+                'backend'      => ''
+            ]
+        );
+    }
+
+
+}
