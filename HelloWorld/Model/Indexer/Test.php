7c468eec3c8f0701a770d39bf7471acca2997b9f
XMars <mars@trueplus.vn>
2018-08-20 14:26:41 +0700

    [ADD] plugins - interceptors

---
+++ HelloWorld/Model/Indexer/Test.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Magestore\HelloWorld\Model\Indexer;
+
+class Test implements \Magento\Framework\Indexer\ActionInterface, \Magento\Framework\Mview\ActionInterface
+{
+    public function execute($ids)
+    {
+
+    }
+
+    public function executeFull()
+    {
+
+    }
+
+    public function executeRow($id)
+    {
+
+    }
+}
